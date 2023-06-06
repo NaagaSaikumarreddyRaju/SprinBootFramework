2e2b499fdb032139a1d9dad6e053821d49cd015b
Saikumar Reddy Raju <sr19adr@herts.ac.uk>
2023-06-06 19:11:59 +0100

    Second Commit

--- src/main/java/com/ias/RequestMapping/TestController.java
+++ src/main/java/com/ias/RequestMapping/TestController.java
@@ -1,6 +1,7 @@
 package com.ias.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,9 +22,19 @@ public class TestController {
 
     @RequestMapping(value = "/student", method = RequestMethod.GET) // we have to return get method in html file if not it will show error
     @ResponseBody
-    public String student(){
+    public String getStudent(){
         return "Students are Good";
     }
+    @RequestMapping(value = "/student", method = RequestMethod.POST) // we have to return get method in html file if not it will show error
+    @ResponseBody
+    public String postStudent(){
+        return "Students are Sleeping";
+    }
 
+    @GetMapping(value = "/empData") // we have to return get method in html file if not it will show error
+    @ResponseBody
+    public String empInfo(){
+        return "Employees are Happy";
+    }
 
 }
